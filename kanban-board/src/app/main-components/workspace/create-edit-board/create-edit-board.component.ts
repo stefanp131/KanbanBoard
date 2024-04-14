5b53d6d096fb7a61175989eee1e058fcc0c081e7
Stefan Popoiu <stefan.popoiu@gmail.com>
2024-04-14 17:03:37 +0300

    Add main components

---
+++ kanban-board/src/app/main-components/workspace/create-edit-board/create-edit-board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-edit-board.component.html',
+  styleUrl: './create-edit-board.component.scss'
+})
+export class CreateEditBoardComponent {
+
+}
